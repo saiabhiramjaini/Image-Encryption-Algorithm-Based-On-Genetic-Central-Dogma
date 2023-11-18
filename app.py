4e0691317dfc38ff48ef2cba31b4b1d1f3af241c
JAINI SAI ABHIRAM <abhiramjaini28@gmail.com>
2023-11-19 01:57:54 +0530

    modified imports

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Image Encryption", page_icon="🔑", layout="wide")
 
